8ae246caf6073b74f5b25867b73925a33cf9d597
Alexsey Simonov <127757884+alWski@users.noreply.github.com>
2025-11-12 03:13:57 +0700

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,15 @@
+lst = list(map(int, input().split()))
+command = input()
+
+napr = command[0]
+pos = int(command[1:])
+
+n = len(lst)
+if napr == 'R':
+    pos = n - pos % n
+elif napr == 'L':
+    pos = pos % n
+
+result = lst[pos:] + lst[:pos]
+
+print(result)
