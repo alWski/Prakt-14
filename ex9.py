38f7eb2b96b67915debe6d308f8846d51947d691
Alexsey Simonov <127757884+alWski@users.noreply.github.com>
2025-11-12 11:56:08 +0700

    Update ex9.py

--- ex9.py
+++ ex9.py
@@ -1,29 +1,36 @@
-textLines = []
-while True:
-    line = input()
-    if not line:
-        break
-    textLines.append(line)
+def proc_text():
+    text_lines = []
+    while True:
+        line = input()
+        if not line:
+            break
+        text_lines.append(line)
 
-text = " ".join(textLines)
+    text = " ".join(text_lines)
 
-punct = ['.', ',', '!', '?', ';', ':', '(', ')']
-words = []
-for word in text.split():
-    newWord = word.lower()
-    for p in punct:
-        newWord = newWord.replace(p, '')
-    if newWord:
-        words.append(newWord)
+    punct = ['.', ',', '!', '?', ';', ':', '(', ')']
+    words = []
+    for word in text.split():
+        new_word = word.lower()
+        for p in punct:
+            new_word = new_word.replace(p, '')
+        if new_word:
+            words.append(new_word)
 
+    col = {}
+    order = []
+    for word in words:
+        if word not in col:
+            col[word] = 0
+            order.append(word)
+        col[word] += 1
 
-col = {}
-order = []
-for word in words:
-    if word not in col:
-        col[word] = 0
-        order.append(word)
-    col[word] += 1
+    result = []
+    for word in sorted(order, key=lambda w: (-col[w], order.index(w))):
+        result.append(word)
+    
+    return result
 
-for word in sorted(order, key=lambda w: (-col[w], order.index(w))):
+result = proc_text()
+for word in result:
     print(word)
