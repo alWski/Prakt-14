9ffa158f9317873f12a4bd460910135ce5a0726e
Alexsey Simonov <127757884+alWski@users.noreply.github.com>
2025-11-12 11:11:28 +0700

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,15 +1,10 @@
-def sumCh():
-    numbers = []
-    for i in range(10):
-        num = int(input())
-        numbers.append(num)
+numbers = []
+for i in range(10):
+    num = int(input())
+    numbers.append(num)
 
-    nList = []
-    for j in range(1, 9):
-        nList.append(numbers[j-1] + numbers[j+1])
+nList = []
+for j in range(1, 9):
+    nList.append(numbers[j-1] + numbers[j+1])
 
-    return nList
-
-result = sumCh()
-print(result)
-
+print(nList)
