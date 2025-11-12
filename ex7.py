56a8941f6f563deae34523ef9f86551af70b93d7
Alexsey Simonov <127757884+alWski@users.noreply.github.com>
2025-11-12 11:52:05 +0700

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -1,12 +1,16 @@
+def sum_all(numbers):
+    chet = 0
+    nechet = 0
+    
+    for i in numbers:
+        if i % 2 == 0:
+            chet += i
+        else:
+            nechet += i
+            
+    return chet, nechet
+
 num = list(map(int, input().split()))
 
-chet = 0
-nechet = 0
-
-for i in num:
-    if i % 2 == 0:
-        chet += i
-    else:
-        nechet += i
-
-print(chet, nechet)
+chet_sum, nechet_sum = sum_all(num)
+print(chet_sum, nechet_sum)
