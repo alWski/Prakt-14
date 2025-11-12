bd95f0e34ac49446cea3017958ce392c698884d7
Alexsey Simonov <127757884+alWski@users.noreply.github.com>
2025-11-12 11:58:38 +0700

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -1,15 +1,20 @@
-lst1 = list(map(int, input().split()))
-lst2 = list(map(int, input().split()))
-start, end = map(int, input().split())
+def proc_list():
+    lst1 = list(map(int, input().split()))
+    lst2 = list(map(int, input().split()))
+    start, end = map(int, input().split())
 
-start = start - 1
-end = end - 1
+    start = start - 1
+    end = end - 1
 
-element = lst1[start:end + 1][::-1]
+    element = lst1[start:end + 1][::-1]
 
-lst2.extend(element)
+    lst2.extend(element)
 
-del lst1[start:end + 1]
+    del lst1[start:end + 1]
 
-print(lst1)
-print(lst2)
+    return lst1, lst2
+
+
+result1, result2 = proc_list()
+print(result1)
+print(result2)
