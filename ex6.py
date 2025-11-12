e52ca212ab33d5b0de5ff81c5af3520630903938
Alexsey Simonov <127757884+alWski@users.noreply.github.com>
2025-11-12 11:46:47 +0700

    Update ex6.py

--- ex6.py
+++ ex6.py
@@ -1,7 +1,9 @@
-num = int(input())
+def find_divisors():
+    num = int(input())
+    delit = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            delit.append(i)
+    print(delit)
 
-delit = []
-for i in range(1, num + 1):
-    if num % i == 0:
-        delit.append(i)
-print(delit)
+find_divisors()
