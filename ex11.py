4d68d613ada3e04f578d2fd0f781d2f71c33fc55
Alexsey Simonov <127757884+alWski@users.noreply.github.com>
2025-11-12 12:00:30 +0700

    Update ex11.py

--- ex11.py
+++ ex11.py
@@ -1,15 +1,19 @@
-lst = list(map(int, input().split()))
-command = input()
+def rotate_list():
+    lst = list(map(int, input().split()))
+    command = input()
 
-napr = command[0]
-pos = int(command[1:])
+    napr = command[0]
+    pos = int(command[1:])
 
-n = len(lst)
-if napr == 'R':
-    pos = n - pos % n
-elif napr == 'L':
-    pos = pos % n
+    n = len(lst)
+    if napr == 'R':
+        pos = n - pos % n
+    elif napr == 'L':
+        pos = pos % n
 
-result = lst[pos:] + lst[:pos]
+    result = lst[pos:] + lst[:pos]
 
+    return result
+
+result = rotate_list()
 print(result)
