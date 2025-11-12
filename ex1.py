c9e70db798023c0e9d6bfe34414f2d4b075226be
Alexsey Simonov <127757884+alWski@users.noreply.github.com>
2025-11-12 11:17:20 +0700

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,10 +1,14 @@
-numbers = []
-for i in range(10):
-    num = int(input())
-    numbers.append(num)
+def sum_num():
+    numbers = []
+    for i in range(10):
+        num = int(input())
+        numbers.append(num)
 
-nList = []
-for j in range(1, 9):
-    nList.append(numbers[j-1] + numbers[j+1])
+    nList = []
+    for j in range(1, 9):
+        nList.append(numbers[j-1] + numbers[j+1])
 
-print(nList)
+    return nList
+
+result = sum_num()
+print(result)
