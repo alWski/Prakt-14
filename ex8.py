c7e28ffbd3c1b6a632b74c070bf3f32ffdef5947
Alexsey Simonov <127757884+alWski@users.noreply.github.com>
2025-11-19 01:53:34 +0700

    Update ex8.py

--- ex8.py
+++ ex8.py
@@ -2,8 +2,11 @@ def sort_string(s):
     nList = list(s)
     nList.sort()
     return ''.join(nList)
+    
+def main():
+    strok = input()
+    res = sort_string(strok)
+    print(res)
 
-
-strok = input()
-res = sort_string(strok)
-print(res)
+if __name__ == "__main__":
+    main()
